229fefd8d0410df3d8982c8261f1276ab79989ce
Matheus Ferreira Geres <matheus.geres@keyrus.com.br>
2019-05-30 15:08:23 -0300

    Remoção das dependências que não eram utilizadas no teste.

--- test/purchase/3-placeorder-creditcard.spec.js
+++ test/purchase/3-placeorder-creditcard.spec.js
@@ -1,7 +1,3 @@
-const should   = require("should");
-const request  = require("request");
-const chai     = require("chai");
-const async    = require("async");
 const logger   = require("mocha-logger");
 const env      = require("../local-food.env");
 const customer = require("../commons/customer");
@@ -10,7 +6,6 @@ const payment  = require("../commons/payment");
 const address  = require("../commons/cart/address");
 const delivery = require("../commons/cart/delivery");
 const product  = require("../commons/product");
-const expect   = chai.expect;
 
 // Ignora a verificação de certificado para Conexões TLS e requests HTTPS. Mais sobre: https://nodejs.org/api/all.html#cli_node_tls_reject_unauthorized_value
 // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
