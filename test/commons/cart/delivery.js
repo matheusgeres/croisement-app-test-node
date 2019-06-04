b76def7ef4746f58342f503c45df0edaabad7d78
Rafael Costa Biasi <rafael.biasi@keyrus.com.br>
2019-06-04 16:56:57 -0300

    Renomando productCodesToDelete para productCodes. Removento requires desnecessários.

--- test/commons/cart/delivery.js
+++ test/commons/cart/delivery.js
@@ -41,43 +41,6 @@ exports.retrieveDeliveryModes = async function (env, requestData) {
   });
 };
 
-exports.retrieveDeliveryModesV2 = async function (env, requestData) {
-  return new Promise((resolve, reject) => {
-    let path = "/app/cart/v2/deliverymodes";
-    request.get(
-        {
-          headers  : requestData.headers,
-          url      : `${requestData.url.foodURL}${requestData.url.siteId}${path}`,
-          strictSSL: env.strictSSL
-        },
-        function (error, response, body) {
-
-          let _body;
-          try {
-            _body = JSON.parse(body);
-            if (env.debug) {
-              logger.log("Body:", JSON.stringify(_body, null, 1));
-            }
-          } catch (e) {
-            _body = {};
-          }
-
-          _body.should.have.property("deliveryModeList");
-          expect(_body.deliveryModeList[0].consignmentCode).to.be.a('string');
-
-          let responseData = {
-            consignmentCode: _body.deliveryModeList[0].consignmentCode
-          };
-          if (error) {
-            reject(error);
-          }
-
-          resolve(responseData);
-        }
-    );
-  });
-};
-
 exports.retrieveDeliverySlots = async function (env, requestData) {
   return new Promise((resolve, reject) => {
     let path = "/app/cart/deliveryslots";
