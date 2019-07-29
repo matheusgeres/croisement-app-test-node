4b089730094f3735ff139070b7cc6a9e82035101
Matheus Ferreira Geres <matheus.geres@keyrus.com.br>
2019-07-29 18:35:33 -0300

    Inserção do teste para aplicação de cupom.

--- test/commons/customer.js
+++ test/commons/customer.js
@@ -1,61 +1,61 @@
-const should  = require("should");
+const should = require("should");
 const request = require("request");
-const chai    = require("chai");
-const logger  = require("mocha-logger");
-const expect  = chai.expect;
+const chai = require("chai");
+const logger = require("mocha-logger");
+const expect = chai.expect;
 
 exports.retrieveToken = async function (env, requestData) {
   return new Promise((resolve, reject) => {
     let path = "/oauth/token";
     // Chaves para realizar a autenticação
     let form = {
-      client_id    : env.token.client_id,
+      client_id: env.token.client_id,
       client_secret: env.token.client_secret,
-      grant_type   : env.token.grant_type,
-      username     : env.token.username,
-      password     : env.token.password,
+      grant_type: env.token.grant_type,
+      username: env.token.username,
+      password: env.token.password,
     };
     if (env.debug) {
       logger.log("Form:", JSON.stringify(form, null, 1));
     }
 
     request.post(
-        {
-          url      : `${requestData.url.urlAuthorization}${path}`,
-          form     : form,
-          strictSSL: env.strictSSL
-        },
-        function (error, response, body) {
+      {
+        url: `${requestData.url.urlAuthorization}${path}`,
+        form: form,
+        strictSSL: env.strictSSL
+      },
+      function (error, response, body) {
 
-          let _body;
-          try {
-            _body = JSON.parse(body);
-            if (env.debug) {
-              logger.log("Body:", JSON.stringify(_body, null, 1));
-            }
-          } catch (e) {
-            _body = {};
-          }
-
-          // Se o token de acesso é adquirido, seta o header com o mesmo.
-          _body.should.have.property("access_token");
-          expect(_body.access_token).to.be.a('string');
-          _body.should.have.property("refresh_token");
-          expect(_body.refresh_token).to.be.a('string');
-
-          let responseData = {
-            authorization: `bearer ${_body.access_token}`
-          };
-
-          if (error) {
-            reject(error);
-          }
+        let _body;
+        try {
+          _body = JSON.parse(body);
           if (env.debug) {
-            logger.log(JSON.stringify(responseData, null, 1));
+            logger.log("Body:", JSON.stringify(_body, null, 1));
           }
-
-          resolve(responseData);
+        } catch (e) {
+          _body = {};
         }
+
+        // Se o token de acesso é adquirido, seta o header com o mesmo.
+        _body.should.have.property("access_token");
+        expect(_body.access_token).to.be.a('string');
+        _body.should.have.property("refresh_token");
+        expect(_body.refresh_token).to.be.a('string');
+
+        let responseData = {
+          authorization: `bearer ${_body.access_token}`
+        };
+
+        if (error) {
+          reject(error);
+        }
+        if (env.debug) {
+          logger.log(JSON.stringify(responseData, null, 1));
+        }
+
+        resolve(responseData);
+      }
     );
   });
 };
@@ -64,38 +64,83 @@ exports.retrieveAllAddress = async function (env, requestData) {
   return new Promise((resolve, reject) => {
     let path = "/app/customer/current/addresses";
     request.get(
-        {
-          headers  : requestData.headers,
-          url      : `${requestData.url.foodURL}${requestData.url.siteId}${path}`,
-          strictSSL: env.strictSSL
-        },
-        function (error, response, body) {
+      {
+        headers: requestData.headers,
+        url: `${requestData.url.foodURL}${requestData.url.siteId}${path}`,
+        strictSSL: env.strictSSL
+      },
+      function (error, response, body) {
 
-          let _body;
-          try {
-            _body = JSON.parse(body);
-            if (env.debug) {
-              logger.log("Body:", JSON.stringify(_body, null, 1));
-            }
-          } catch (e) {
-            _body = {};
-          }
-
-          _body.should.have.property("addresses");
-          expect(_body.addresses[0].id).to.be.a('string');
-
-          const responseData = {
-            addressId: _body.addresses[0].id
-          };
-
-          if (error) {
-            reject(error);
-          }
+        let _body;
+        try {
+          _body = JSON.parse(body);
           if (env.debug) {
-            logger.log(JSON.stringify(responseData, null, 1));
+            logger.log("Body:", JSON.stringify(_body, null, 1));
           }
-          resolve(responseData);
+        } catch (e) {
+          _body = {};
         }
+
+        _body.should.have.property("addresses");
+        expect(_body.addresses[0].id).to.be.a('string');
+
+        const responseData = {
+          addressId: _body.addresses[0].id
+        };
+
+        if (error) {
+          reject(error);
+        }
+        if (env.debug) {
+          logger.log(JSON.stringify(responseData, null, 1));
+        }
+        resolve(responseData);
+      }
     );
   });
 }
+
+exports.applyVoucher = async function (env, requestData) {
+  let path = "/app/cart/apply-voucher";
+  let form = {
+    voucherCode: requestData.voucher,
+    cartCode: env.cartCode,
+  };
+  return new Promise((resolve, reject) => {
+    request.post(
+      {
+        headers: requestData.headers,
+        url: `${requestData.url.foodURL}${requestData.url.siteId}${path}`,
+        form: form,
+        strictSSL: env.strictSSL
+      },
+      function (error, response, body) {
+
+        let _body;
+        try {
+          _body = JSON.parse(body);
+          if (env.debug) {
+            logger.log("Body:", JSON.stringify(_body, null, 1));
+          }
+        } catch (e) {
+          _body = {};
+        }
+
+        logger.error("RESPONSE OF VOUCHER", _body);
+
+        if (error) {
+          reject(error);
+        }
+
+        const responseData = {
+        };
+
+        if (env.debug) {
+          logger.log(JSON.stringify(responseData, null, 1));
+        }
+
+        resolve(responseData);
+      }
+    );
+  });
+};
---
+++ test/purchase/3-one-product-vouncher.spec.js
@@ -0,0 +1,135 @@
+const logger   = require("mocha-logger");
+const env      = require("../local-food.env");
+const customer = require("../commons/customer");
+const cart     = require("../commons/cart");
+const payment  = require("../commons/payment");
+const address  = require("../commons/cart/address");
+const delivery = require("../commons/cart/delivery");
+
+// Ignora a verificação de certificado para Conexões TLS e requests HTTPS. Mais sobre: https://nodejs.org/api/all.html#cli_node_tls_reject_unauthorized_value
+// process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+
+const urlBase          = env.urlBase;
+const urlCrfws         = `${urlBase}/crfws`;
+const urlAuthorization = `${urlBase}/authorizationserver`;
+const foodURL          = `${urlBase}/foodws`;
+const siteId           = "/v2/carrefour";
+
+const url = {
+  urlBase         : urlBase,
+  urlCrfws        : urlCrfws,
+  urlAuthorization: urlAuthorization,
+  foodURL         : foodURL,
+  siteId          : siteId
+};
+
+describe("Make a purchase with one product", function () {
+  // Variáveis globais que são reutilizadas entre as chamadas.
+  let headers              = {};url
+  let productCodesToDelete = [];
+  let cartCode;
+  let addressId;
+  let consignmentCode;
+  let slotCode;
+  let slotDate;
+
+  step("Retrieve (Get) customer token", async function (done) {
+
+    const requestData = {url: url};
+
+    const responseData       = await customer.retrieveToken(env, requestData);
+    headers['Authorization'] = responseData.authorization;
+
+    done();
+  });
+
+  step("Retrieve Cart", async function (done) {
+    const requestData = {
+      url    : url,
+      headers: headers
+    };
+
+    const responseData   = await cart.retrieveCart(env, requestData);
+    productCodesToDelete = responseData.productCodes;
+    cartCode             = responseData.cartCode;
+
+    done();
+  });
+
+  step("Clear Cart", async function (done) {
+    if (productCodesToDelete.length === 0) {
+      return done();
+    }
+
+    const requestData = {
+      url         : url,
+      headers     : headers,
+      cartCode    : cartCode,
+      productCodes: productCodesToDelete
+    };
+
+    await cart.clearCart(env, requestData);
+
+    done();
+  });
+
+  xstep("Retrieve All Addresses", async function (done) {
+    if (env.deliveryAddress.force) {
+      addressId = env.deliveryAddress.addressId;
+      return done();
+    }
+
+    const requestData = {
+      url    : url,
+      headers: headers
+    };
+
+    let responseData = await customer.retrieveAllAddress(env, requestData);
+    addressId        = responseData.addressId;
+
+    done();
+  });
+
+  xstep("Set Delivery Address", async function (done) {
+    const requestData = {
+      url      : url,
+      headers  : headers,
+      cartCode : cartCode,
+      addressId: addressId
+    };
+
+    await address.setDeliveryAddress(env, requestData);
+
+    done();
+  });
+
+  xstep("Add Product to Cart", async function (done) {
+    const requestData = {
+      url         : url,
+      headers     : headers,
+      productCodes: [env.product.productCode],
+      quantity    : env.product.quantity
+    };
+
+    await cart.addProducts(env, requestData);
+
+    done();
+
+  });
+
+  step("Apply voucher", async function (done) {
+
+    const requestData = {
+      url    : url,
+      headers: headers,
+      voucher: env.promotion.voucherCode
+    };
+
+    let responseData = await customer.applyVoucher(env, requestData);
+   
+    done();
+  });
+
+
+  after(() => logger.log('cartCode ==> ', cartCode));
+});
